dee923d284901da10aeabe840390c8423548dbc8
adans0102 <aylindasanche@gmail.com>
2025-11-06 08:37:03 -0800

    agregue detail con aplicacion store

--- store/views.py
+++ store/views.py
@@ -1,3 +1,34 @@
+
 from django.shortcuts import render
+from .models import Item, Category
+from django.shortcuts import get_object_or_404
+
 
 # Create your views here.
+def home (request):
+    items = Item.objects.filter(is_sold=False)
+    categories = Category.objects.all()
+
+
+    context = {
+        'items': items,
+        'categories': categories
+    }
+    return render(request, 'store/home.html', context)
+
+
+
+
+
+
+def detail(request,pk):
+    item = get_object_or_404(Item, pk=pk)
+    related_items = item.objects.filter(Category=item.category,
+                                        is_sold=False).exclude(pk=pk)[0:3]
+    
+    context={
+    "item":item,
+     "related_items": related_items
+    }
+
+return render(request, "store/item.html", context)
